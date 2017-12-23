5971c92582b7d0c050de05d18ed9b48250b0e5ad
Vinh Nguyen <nguyen.kvvn@gmail.com>
2017-12-23 03:10:03 -0500

    Baseline framework
    Built from
    https://alanbondo.wordpress.com/2008/06/22/creating-a-system-tray-app-with-c/

---
+++ Bonbon/Bonbon/Bonbon.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+//using guide from https://alanbondo.wordpress.com/2008/06/22/creating-a-system-tray-app-with-c/
+namespace Bonbon
+{
+    class Bonbon : Form
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.Run(new Bonbon());
+        }
+
+        private NotifyIcon trayIcon;
+        private ContextMenu trayMenu;
+
+        public Bonbon()
+        {
+            // Create a simple tray menu with only one item.
+            trayMenu = new ContextMenu();
+            trayMenu.MenuItems.Add("Exit", OnExit);
+
+            // Create a tray icon. In this example we use a
+            // standard system icon for simplicity, but you
+            // can of course use your own custom icon too.
+            trayIcon = new NotifyIcon();
+            trayIcon.Text = "Bonbon";
+            trayIcon.Icon = new Icon(SystemIcons.Application, 40, 40);
+
+            // Add menu to tray icon and show it.
+            trayIcon.ContextMenu = trayMenu;
+            trayIcon.Visible = true;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            Visible = false; // Hide form window.
+            ShowInTaskbar = false; // Remove from taskbar.
+
+            base.OnLoad(e);
+        }
+
+        private void OnExit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            if (isDisposing)
+            {
+                // Release the icon resource.
+                trayIcon.Dispose();
+            }
+
+            base.Dispose(isDisposing);
+        }
+    }
+}
